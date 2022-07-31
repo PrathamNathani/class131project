a1d90490e6ce18b54b8dc244f62a4178fa751843
PrathamNathani <73383584+PrathamNathani@users.noreply.github.com>
2022-07-31 16:45:43 +0530

    Add files via upload

---
+++ home_main.js
@@ -0,0 +1,16 @@
+function bedroom_btn(){
+    window.location="bedroom_index.html";
+}
+
+function livingRoom_btn(){
+    window.location="livingroom_index.html";
+}
+
+function gamingRoom_btn(){
+    window.location="gamingroom_index.html";
+}
+
+function kitchen_btn(){
+    window.location="kitchen_index.html";
+}
+
---
+++ livingroom_main.js
@@ -0,0 +1,52 @@
+var img="";
+var status_img="";
+var objects=[];
+
+function preload(){
+    img=loadImage("livingroom.jpg");
+    
+    }
+
+    function setup(){
+        canvas=createCanvas(480,380);
+        canvas.center();
+        object_detector=ml5.objectDetector("cocossd",modelLoaded);
+        document.getElementById("status_object").innerHTML="Status: Detecting Objects";
+        }
+
+        function draw(){
+            image(img,0,0,480,380);
+            if(status_img!=""){
+                r=random(255);
+                g=random(255);
+                b=random(255);
+                for(var i=0;i<objects.length;i++){
+                   document.getElementById("status").innerHTML="Status: Objects Detected";
+                   fill("red");
+                   percentage=floor(objects[i].confidence *100);
+                   text(objects[i].label+" "+percentage+"%",objects[i].x+15,objects[i].y+15);
+                   noFill();
+                   stroke(r,g,b);
+                   rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+                }
+               }
+           
+            }
+            function back(){
+                window.location="home_index.html";
+            }
+            function modelLoaded(){
+            console.log("modelLoaded");
+            status_img=true;
+            object_detector.detect(img,gotresult);
+            }
+            
+            function gotResult(error,results){
+                if(error){
+                    console.log(error);
+                }
+                else{
+                    console.log(results);
+                    objects=results;
+                }
+            }
